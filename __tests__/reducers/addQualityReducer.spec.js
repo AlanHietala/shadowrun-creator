ecaa7c54b6a1274d73136bb62a0970775656aca4
Alan Hietala <alan.hietala@gmail.com>
2017-01-09 00:09:08 -0500

    updating quality reducers

---
+++ __tests__/reducers/addQualityReducer.spec.js
@@ -0,0 +1,56 @@
+import addQualityReducer from '../../app/js/reducers/addQualityReducer';
+import {ADD_QUALITY, REMOVE_QUALITY} from '../../app/js/constants/qualityActionTypes';
+
+describe('quality Reducer', function () {
+	it('should add a quality to the list referenced by the payload', function () {
+		const initialState = {
+			qualities: [],
+			karma: 20
+		}
+		const stateAfter = {
+			qualities: [
+				{
+					name: 'Astral Chameleon',
+					karmaCost: 10
+				}
+			],
+			karma: 10
+		}
+
+		const action = {
+			type: ADD_QUALITY,
+			payload: {
+				name: 'Astral Chameleon',
+				karmaCost: 10
+			}
+
+		}
+		expect(addQualityReducer(initialState, action)).toEqual(stateAfter);
+	});
+
+	it('should not add a quality if the cost is greater than the available karma', function () {
+		const initialState = {
+			qualities: [],
+			karma: 8
+		}
+		const stateAfter = {
+			qualities: [],
+			karma: 8
+		}
+
+		const action = {
+			type: ADD_QUALITY,
+			payload: {
+				name: 'Astral Chameleon',
+				karmaCost: 10
+			}
+
+		}
+		expect(addQualityReducer(initialState, action)).toEqual(stateAfter);
+	});
+
+
+
+
+
+});
---
+++ __tests__/reducers/removeQualityReducer.spec.js
@@ -0,0 +1,36 @@
+import removeQualityReducer from '../../app/js/reducers/removeQualityReducer';
+import {REMOVE_QUALITY} from '../../app/js/constants/qualityActionTypes';
+
+describe('removeQualityReducer', function () {
+	it('should remove a quality at the index in the payload and refund the karma', function () {
+		const initialState = {
+			qualities: [
+				{
+					name: 'Astral Chameleon',
+					karmaCost: 10
+				},
+				{
+					name: 'Bar Cart',
+					karmaCost: 7
+				}
+			],
+			karma: 20
+		}
+		const stateAfter = {
+			qualities: [
+				{
+					name: 'Astral Chameleon',
+					karmaCost: 10
+				}
+			],
+			karma: 27
+		}
+
+		const action = {
+			type: REMOVE_QUALITY,
+			payload: 1
+		}
+		expect(removeQualityReducer(initialState, action)).toEqual(stateAfter);
+	});
+
+});
