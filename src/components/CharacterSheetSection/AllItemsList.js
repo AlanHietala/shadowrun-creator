d6ee8631434c5f045ee9020708f014d6134e9d35
Alan Hietala <alan.hietala@gmail.com>
2017-08-06 23:55:52 -0400

    adding search to all item type components

--- src/components/CharacterSheetSection/AllItemsList.js
+++ src/components/CharacterSheetSection/AllItemsList.js
@@ -3,14 +3,44 @@ import Subheader from 'material-ui/Subheader'
 import List from 'material-ui/List'
 import AddItem from './AddItem'
 import PropTypes from 'prop-types'
+import TextField from 'material-ui/TextField'
 
-const AllItemsList = ({title, allItems, handleAddItem, ItemComponent}) => {
-  const itemList = allItems.map((item, index) => <AddItem key={index} item={item} handleAddItem={handleAddItem} ItemComponent={ItemComponent} />)
-  return (<List>
-    <Subheader>{title}</Subheader>
-    {itemList}
-  </List>)
+class AllItemsList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filterString: '',
+    }
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      filterString: e.target.value,
+    })
+  }
+
+  render() {
+    const {title, allItems, handleAddItem, ItemComponent} = this.props
+    const filteredItems = allItems.filter(item => this.state.filterString === ''
+      || item.name.toLowerCase().indexOf(this.state.filterString) > -1)
+    const itemList = filteredItems
+      .map((item, index) => (<AddItem key={index}
+        item={item}
+        handleAddItem={handleAddItem}
+        ItemComponent={ItemComponent}
+      />))
+
+    return (<List>
+      <Subheader>{title}</Subheader>
+      <TextField
+        name={"search"}
+        hintText={"search"}
+        onChange={this.handleOnChange} />
+      {itemList}
+    </List>)
+  }
 }
+
 AllItemsList.propTypes = {
   title: PropTypes.string,
   allItems: PropTypes.array,
