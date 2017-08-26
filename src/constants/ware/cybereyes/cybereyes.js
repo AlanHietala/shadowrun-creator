978c3fb4e1888bd44413f06fa95a85d4ac48a6cc
Alan Hietala <alan.hietala@gmail.com>
2017-08-26 19:57:23 -0400

    updating earware (#20)

--- src/constants/ware/cybereyes/cybereyes.js
+++ src/constants/ware/cybereyes/cybereyes.js
@@ -1,17 +1,6 @@
 import * as modTypes from '../../modTypes'
+import {installableOption} from '../wareHelpers'
 
-const installableOption = (option) => {
-  const essenceIndex = option.mods.findIndex(mod => mod.modType === modTypes.ESSENCE_MOD)
-  const mods = [
-    ...option.mods.slice(0, essenceIndex),
-    ...option.mods.slice(essenceIndex + 1),
-  ]
-  return {
-    ...option,
-    mods,
-    isInstalled: false,
-  }
-}
 
 const flareCompensation = {
   key: 'flareCompensation',
